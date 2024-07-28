09a9547c6032e1a3fe817dd36f1784192ad5ef44
Anas2018EMI <anassaatef@gmail.com>
2024-07-28 09:03:11 -0700

    Initial commit

--- 0-printf.c
+++ 0-printf.c
@@ -12,30 +12,27 @@
  * @sum: second arg
  * @a: third arg
  * @to: seventh arg
- * @is: eighth arg
  * @format: First arg
  *
  * Return: int
  */
-int check_str(int i, int sum, int a, va_list to, bool is, const char *format)
+int check_str(int i, int sum, int a, va_list to, const char *format)
 {
 	int num;
-	char c;
-	char *str;
+	bool is = 0;
 
 	for (i = 0; i < a; i++)
 	{
+		is = (*(format + i) == 'd' || *(format + i) == 'i');
 		if (i > 0 && *(format + i - 1) == '%' && *(format + i) == 'c')
 		{
-			c = va_arg(to, int);
-			print_char(c);
+			print_char(va_arg(to, int));
 			sum++;
 		}
 		else if (i > 0 && *(format + i - 1) == '%' && *(format + i) == 's')
 		{
-			str = va_arg(to, char *);
-			print_str(str);
-			sum += _len(str);
+			print_str(va_arg(to, char *));
+			sum += _len(va_arg(to, char *));
 		}
 		else if (i > 0 && *(format + i - 1) == '%' && is)
 		{
@@ -71,7 +68,6 @@ int _printf(const char *format, ...)
 {
 	int i = 0, sum = 0, length;
 	va_list to;
-	bool is = *(format + i) == 'd' || *(format + i) == 'i';
 
 	if (format == NULL)
 	{
@@ -79,8 +75,7 @@ int _printf(const char *format, ...)
 	}
 	length = _len(format);
 	va_start(to, format);
-	sum = check_str(i, sum, length, to, is, format);
+	sum = check_str(i, sum, length, to, format);
 	va_end(to);
 	return (sum);
 }
-
