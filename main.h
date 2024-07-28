2777c8487e76c68da9e6247e11863a78d397d5bb
Anas2018EMI <anassaatef@gmail.com>
2024-07-28 10:47:49 -0700

    Initial commit

--- main.h
+++ main.h
@@ -6,7 +6,6 @@
 #include <stdlib.h>
 int print_char(char c);
 int _printf(const char *format, ...);
-int _len(const char *str);
 int print_str(char *str);
 int print_number(int num);
 int handle_format(char specifier, va_list args);
